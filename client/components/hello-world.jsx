aca5250306eef9fde6f560533b43969efd822f69
Joseph Bravo <josephangelobravo@gmail.com>
2022-05-30 19:56:44 +0000

    integrated TailwindCSS and DaisyUI

--- client/components/hello-world.jsx
+++ client/components/hello-world.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function HelloWorld(props) {
   return (
-    <h1>Hello, World!</h1>
+    <h1 className='bg-primary text-primary-content  font-black text-4xl'>Hello, World!</h1>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ require('dotenv/config');
 const path = require('path');
 const webpack = require('webpack');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const clientPath = path.join(__dirname, 'client');
 const serverPublicPath = path.join(__dirname, 'server', 'public');
@@ -28,9 +29,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [
-              '@babel/preset-env'
-            ],
+            presets: ['@babel/preset-env'],
             plugins: [
               '@babel/plugin-transform-react-jsx',
               isDevelopment && 'react-refresh/babel'
@@ -41,8 +40,19 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          'style-loader',
-          'css-loader'
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: {
+                  tailwindcss: {},
+                  autoprefixer: {}
+                }
+              }
+            }
+          }
         ]
       }
     ]
@@ -50,6 +60,7 @@ module.exports = {
   stats: 'minimal',
   devtool: isDevelopment ? 'cheap-module-source-map' : 'source-map',
   plugins: [
+    new MiniCssExtractPlugin(),
     new webpack.EnvironmentPlugin([]),
     isDevelopment && new ReactRefreshWebpackPlugin(),
     isDevelopment && new webpack.NoEmitOnErrorsPlugin(),
