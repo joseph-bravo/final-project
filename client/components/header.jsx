2fe458655d977e88eda6412da292c98394626a36
Joseph Bravo <josephangelobravo@gmail.com>
2022-06-02 19:55:43 +0000

    adjusted header to match route and added proper navigation

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,43 +1,85 @@
 import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 
-export default class Header extends React.Component {
-  render() {
-    return (
-      <header
-        className="navbar rounded-box sticky
+export default function Header(props) {
+  const { pathname } = useLocation();
+  /**
+   * Key: Path
+   * Header: String to display on Header and Menu
+   */
+  const titles = {
+    '/': {
+      header: 'Symbol Vault'
+    },
+    '/upload': {
+      header: 'New Post'
+    }
+  };
+
+  const actions = [titles['/upload']];
+
+  return (
+    <header
+      className="navbar rounded-box sticky
         top-4 mx-auto mb-8 flex w-11/12 max-w-3xl items-center
-        justify-between bg-primary px-4 text-primary-content shadow-md">
-        <div className="">
-          <button className="btn btn-primary">
-            <span className="material-icons">arrow_back</span>
-          </button>
-        </div>
+        justify-center bg-primary px-4 text-primary-content shadow-md">
+      <div className="justify-left flex-1 flex-shrink md:flex-none">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            !isActive ? 'btn btn-primary' : 'hidden'
+          }>
+          <span className="material-icons">home</span>
+        </NavLink>
+      </div>
 
-        <div className="flex-1 justify-center md:justify-start md:px-2">
-          <h1 className="text-center text-4xl font-black leading-none">
-            New Post
-          </h1>
-        </div>
+      <div className="flex-auto justify-center md:justify-start md:px-4">
+        <h1 className="text-center text-4xl font-black leading-none">
+          {titles[pathname].header}
+        </h1>
+      </div>
 
-        <div className="md:hidden">
-          <div className="dropdown dropdown-end">
-            <label tabIndex="0" className="btn btn-primary">
-              <span className="material-icons">menu</span>
-            </label>
-            <ul
-              tabIndex="0"
-              className="dropdown-content menu rounded-box mt-4 w-52 bg-primary-focus p-2 shadow">
-              <li>
-                <a>log out</a>
-              </li>
-            </ul>
-          </div>
+      <div className="flex flex-1 justify-end md:hidden">
+        <div className="dropdown-end dropdown">
+          <label tabIndex="0" className="btn btn-primary">
+            <span className="material-icons">menu</span>
+          </label>
+          <ul
+            tabIndex="0"
+            className="dropdown-content menu rounded-box mt-4 w-52 bg-primary-focus p-2 shadow">
+            {
+              // prettier-ignore
+              actions.map(action => (
+                <NavLink
+                  key={action}
+                  to="upload"
+                  className={({ isActive }) =>
+                    !isActive ? 'btn btn-primary text-xl lowercase' : 'hidden'
+                  }>
+                  {action.header}
+                </NavLink>
+              ))
+            }
+          </ul>
         </div>
+      </div>
 
-        <div className="hidden md:flex">
-          <button className="btn btn-primary text-xl lowercase">Log Out</button>
-        </div>
-      </header>
-    );
-  }
+      <ul className="hidden flex-1 justify-end md:flex">
+        {
+          // prettier-ignore
+          actions.map(action => (
+            <NavLink
+              key={action}
+              to="upload"
+              className={({ isActive }) =>
+                !isActive ? 'btn btn-primary text-xl lowercase' : 'hidden'
+              }>
+              {action.header}
+          </NavLink>
+          ))
+        }
+        <li></li>
+      </ul>
+    </header>
+  );
 }
