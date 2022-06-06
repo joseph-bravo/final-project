7517627bda145ed17482e3a23f1e6f2444669e93
Joseph Bravo <josephangelobravo@gmail.com>
2022-06-06 20:21:35 +0000

    create /api/posts/search endpoint created savestate

--- server/index.js
+++ server/index.js
@@ -123,6 +123,112 @@ app.get('/api/posts/view/:id', (req, res, next) => {
 });
 
 /**
+ * ? Searches DB for posts matching the query details.
+ * @QueryParameter q - text of the search query.
+ * @QueryParameter cols - columns to apply search to.
+ */
+app.get('/api/posts/search', (req, res, next) => {
+  const { q: query, cols: columns } = req.query;
+  if (!query) {
+    throw new ClientError(
+      400,
+      'provide query param "q" with text you want to search for'
+    );
+  }
+  if (!columns) {
+    throw new ClientError(
+      400,
+      'provide query param "cols" with columns you want to search for (title, description, tags, username) separated by ,'
+    );
+  }
+  const columnsArray = columns.split(',');
+  const columnsConfig = {
+    title: columnsArray.includes('title'),
+    description: columnsArray.includes('description'),
+    tags: columnsArray.includes('tags'),
+    username: columnsArray.includes('username')
+  };
+
+  if (
+    !columnsConfig.title &&
+    !columnsConfig.description &&
+    !columnsConfig.tags &&
+    !columnsConfig.username
+  ) {
+    throw new ClientError(
+      400,
+      'provide query param "cols" with columns you want to search for (title, description, tags, username) separated by ,'
+    );
+  }
+
+  // prettier-ignore
+  const sql = `/* SQL */
+    with "tag_arrays" as (
+      select
+        "postId",
+        array_to_string(array_agg("tagName"), ' ') as "tags"
+      from "taggings"
+      group by "postId"
+    ), "posts_with_tags" as (
+      select * from "posts"
+      join "users" using ("userId")
+      join "tag_arrays" using ("postId")
+    ), "weighted_posts" as (
+      select
+        "postId",
+        ${
+          columnsConfig.title
+            ? `/* SQL */
+            setweight(to_tsvector("title"), 'A') ||`
+            : ''
+        }
+        ${
+          columnsConfig.description
+            ? `/* SQL */
+            setweight(to_tsvector("description"), 'B') ||`
+            : ''
+        }
+        ${
+          columnsConfig.tags
+            ? `/* SQL */
+            setweight(to_tsvector("tags"), 'C') ||`
+            : ''
+        }
+        ${
+          columnsConfig.username
+            ? `/* SQL */
+            setweight(to_tsvector("username"), 'D') ||`
+            : ''
+        }
+        ''
+          as "weights"
+      from "posts_with_tags"
+    ), "search_ranking" as (
+      select *, ts_rank("weights", plainto_tsquery($1)) as "ranks"
+      from "weighted_posts"
+    )
+
+    select
+      "title", "description", "username",
+      "fileObjectKey", "previewImagePath",
+      "filePropsName", "filePropsSound", "filePropsLayerCount",
+      "postId", "userId", "posts"."createdAt", "tags", "ranks"
+    from "search_ranking"
+    join "posts" using ("postId")
+    join "files" using ("fileId")
+    join "users" using ("userId")
+    join "tag_arrays" using ("postId")
+    where "ranks" > 0.01
+    order by "ranks" desc
+    limit 20
+    ;
+  `;
+  db.query(sql, [query])
+    .then(reSQL => res.json(reSQL.rows))
+    .catch(next);
+});
+
+/**
  * ? Redirects to a download of the SAR file given the ID of post.
  */
 app.get('/api/posts/download/:id', (req, res, next) => {
