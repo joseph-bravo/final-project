929fde8c2888b461317ebbfae57e2158602c20ee
Joseph Bravo <josephangelobravo@gmail.com>
2022-06-07 00:13:37 +0000

    updating log with new posts add alert to signify current query with button to reset handling no visible posts cleanup and ready for PR

--- client/components/search-bar.jsx
+++ client/components/search-bar.jsx
@@ -12,11 +12,18 @@ export default class SearchBar extends React.Component {
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.clearFormAndInitialize = this.clearFormAndInitialize.bind(this);
   }
 
   handleSubmit(e) {
     e.preventDefault();
     const { q, title, description, tags, username } = this.state;
+
+    if (q.trim() === '') {
+      this.props.initializeCatalog();
+      return;
+    }
+
     const cols = [
       title ? 'title' : '',
       description ? 'description' : '',
@@ -26,10 +33,20 @@ export default class SearchBar extends React.Component {
     const searchParams = new URLSearchParams('');
     searchParams.append('q', q);
     searchParams.append('cols', cols);
-    console.log('/api/posts/search?' + searchParams.toString());
     fetch('/api/posts/search?' + searchParams.toString())
       .then(res => res.json())
-      .then(console.log);
+      .then(res => this.props.setCatalog(res, q.trim()));
+  }
+
+  clearFormAndInitialize() {
+    this.setState({
+      q: '',
+      title: true,
+      description: true,
+      tags: true,
+      username: true
+    });
+    this.props.initializeCatalog();
   }
 
   handleChange(event) {
@@ -56,65 +73,77 @@ export default class SearchBar extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="rounded-box flex items-center gap-16 bg-base-100 px-4 py-2 shadow-sm">
-          <div className="form-control w-full">
-            <div className="input-group ">
-              <input
-                type="text"
-                placeholder="search..."
-                className="input input-bordered w-full"
-                name="q"
-                value={this.state.q}
-                onChange={this.handleChange}
-                required
-              />
-              <button className="btn btn-square">
-                <span className="material-icons bg-transparent p-0">
-                  search
-                </span>
-              </button>
+      <div className="flex flex-col gap-2">
+        <form onSubmit={this.handleSubmit}>
+          <div className="rounded-box flex items-center gap-4 bg-base-100 px-4 py-2 shadow-sm">
+            <div className="form-control w-full">
+              <div className="input-group ">
+                <input
+                  type="text"
+                  placeholder="search..."
+                  className="input input-bordered w-full"
+                  name="q"
+                  value={this.state.q}
+                  onChange={this.handleChange}
+                />
+                <button className="btn btn-square">
+                  <span className="material-icons bg-transparent p-0">
+                    search
+                  </span>
+                </button>
+              </div>
+            </div>
+            <div className="dropdown-end dropdown">
+              <label tabIndex="0" className="btn m-1">
+                options
+              </label>
+              <ul
+                tabIndex="0"
+                className="dropdown-content rounded-box w-52 bg-base-100 p-2 shadow">
+                <li>
+                  <DaisyToggle
+                    label="title"
+                    toggled={this.state.title}
+                    onChange={this.handleChange}
+                  />
+                </li>
+                <li>
+                  <DaisyToggle
+                    label="description"
+                    toggled={this.state.description}
+                    onChange={this.handleChange}
+                  />
+                </li>
+                <li>
+                  <DaisyToggle
+                    label="tags"
+                    toggled={this.state.tags}
+                    onChange={this.handleChange}
+                  />
+                </li>
+                <li>
+                  <DaisyToggle
+                    label="username"
+                    toggled={this.state.username}
+                    onChange={this.handleChange}
+                  />
+                </li>
+              </ul>
             </div>
           </div>
-          <div className="dropdown-end dropdown">
-            <label tabIndex="0" className="btn m-1">
-              options
-            </label>
-            <ul
-              tabIndex="0"
-              className="dropdown-content rounded-box w-52 bg-base-100 p-2 shadow">
-              <li>
-                <DaisyToggle
-                  label="title"
-                  toggled={this.state.title}
-                  onChange={this.handleChange}
-                />
-              </li>
-              <li>
-                <DaisyToggle
-                  label="description"
-                  toggled={this.state.description}
-                  onChange={this.handleChange}
-                />
-              </li>
-              <li>
-                <DaisyToggle
-                  label="tags"
-                  toggled={this.state.tags}
-                  onChange={this.handleChange}
-                />
-              </li>
-              <li>
-                <DaisyToggle
-                  label="username"
-                  toggled={this.state.username}
-                  onChange={this.handleChange}
-                />
-              </li>
-            </ul>
-          </div>
-        </div>
-      </form>
+        </form>
+        {
+          // prettier-ignore
+          this.props.currentQuery
+            ? <div className='alert alert-info max-w-md py-2'>
+                  <div>
+                    <h2>showing matches for &quot;<span className="font-bold">{this.props.currentQuery}</span>&quot;</h2>
+                  </div>
+                  <button className='btn btn-circle btn-ghost' onClick={this.clearFormAndInitialize}><div className="material-icons">close</div></button>
+                </div>
+            : <></>
+        }
+      </div>
     );
   }
 }
--- client/pages/catalog.jsx
+++ client/pages/catalog.jsx
@@ -7,28 +7,40 @@ export default class CatalogPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentlyViewing: []
+      currentlyViewing: [],
+      showingMatchesFor: ''
     };
     this.setCatalog = this.setCatalog.bind(this);
+    this.initializeCatalog = this.initializeCatalog.bind(this);
   }
 
   componentDidMount() {
+    this.initializeCatalog();
+  }
+
+  initializeCatalog() {
     fetch('/api/catalog')
       .then(res => res.json())
       .then(res => {
-        this.setState({ currentlyViewing: res });
+        this.setCatalog(res);
       })
       .catch(console.error);
   }
 
-  setCatalog(posts) {
-    this.setState({ currentlyViewing: posts });
+  setCatalog(posts, currentQuery) {
+    this.setState({ currentlyViewing: posts, currentQuery });
   }
 
   render() {
     return (
       <div className="flex flex-col items-center gap-8">
-        <SearchBar setCatalog={this.setCatalog} />
+        <div>
+          <SearchBar
+            setCatalog={this.setCatalog}
+            initializeCatalog={this.initializeCatalog}
+            currentQuery={this.state.currentQuery}
+          />
+        </div>
         <div className="grid gap-4 sm:grid-cols-2">
           {this.state.currentlyViewing.map(symbolArt => {
             return (
@@ -36,6 +48,12 @@ export default class CatalogPage extends React.Component {
             );
           })}
         </div>
+        {
+          // prettier-ignore
+          !(this.state.currentlyViewing.length > 0)
+            ? <div className="alert alert-warning w-fit">unable to find anything.</div>
+            : null
+        }
       </div>
     );
   }
