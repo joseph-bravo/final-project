d1f5cb66e12e1af9a9895a12dc7b7346731cc787
Joseph Bravo <josephangelobravo@gmail.com>
2022-06-08 18:58:33 +0000

    added jwtdecode lib created daisy-modal.jsx component to use for any future modals clientside login with token stored in local storage decoding token upon login and refresh proper error checking for signin/signup added usertoken and username to app context spread state props in app context rather than storing a direct reference to it

--- client/app.jsx
+++ client/app.jsx
@@ -8,16 +8,50 @@ import AppContext from './lib/app-context';
 import SinglePostPage from './pages/single-post';
 import NotFoundPage from './pages/not-found';
 import AuthPage from './pages/auth';
+import jwtDecode from 'jwt-decode';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      userToken: null,
+      username: null
+    };
     this.playSound = this.playSound.bind(this);
     this.sounds = sounds.map(sound => {
       if (sound.path === '') return null;
       return new Audio(`/res/${sound.path}`);
     });
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  componentDidMount() {
+    const userjwt = localStorage.getItem('userjwt');
+    if (userjwt && userjwt !== 'undefined') {
+      this.login(userjwt);
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.userToken !== this.state.userToken) {
+      if (!this.state.userToken) {
+        this.setState({ username: null });
+        return;
+      }
+      const { username } = jwtDecode(this.state.userToken);
+      this.setState({ username });
+    }
+  }
+
+  login(userToken) {
+    localStorage.setItem('userjwt', userToken);
+    this.setState({ userToken });
+  }
+
+  logout() {
+    localStorage.removeItem('userjwt');
+    this.setState({ userToken: null });
   }
 
   playSound(index) {
@@ -40,9 +74,10 @@ export default class App extends React.Component {
 
   render() {
     const context = {
-      state: this.state,
-      setState: this.setState,
-      playSound: this.playSound
+      ...this.state,
+      playSound: this.playSound,
+      login: this.login,
+      logout: this.logout
     };
     return (
       <AppContext.Provider value={context}>
